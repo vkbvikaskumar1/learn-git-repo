aa8df4cc474aebfb1351bb6e355a02603dcbc0c6
vikas <vkbvikaskumar1@gmail.com>
2020-01-22 20:42:51 +0530

    updated for loops

--- forloop.cpp
+++ forloop.cpp
@@ -3,5 +3,8 @@ using namespace std;
 int main()
 {
 	for(int i=0;i<5;i++) cout<<"GOOGLE"<<endl;
+		cout<<"done with for loops"<<endl;
+
+		cout<<"done with for loops seriously!!"<<endl;
 return 0;
 }
