a561b6d9b49f61a8015530fa834aa7af9083ca7b
vikas <vkbvikaskumar1@gmail.com>
2020-01-22 20:36:17 +0530

    initial commit done

---
+++ forloop.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	for(int i=0;i<5;i++) cout<<"GOOGLE"<<endl;
+return 0;
+}
---
+++ hellodemo.cpp
@@ -0,0 +1,7 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+	cout<<"HELLO VIKAS";
+return 0;
+}
